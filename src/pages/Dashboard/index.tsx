4593390782dfa8f4ccd9422d1037975d7783dd22
annemoraes <ilarymoraes@hotmail.com>
2021-01-30 00:31:47 -0300

    consumindo a api para listar gifs pela pesquisa

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,59 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState, FormEvent } from 'react';
+import { Container, Form } from './styles';
+import api from '../../services/api';
+
+interface Gif {
+  id: string;
+  title: string;
+  images: {
+    fixed_height: {
+      url: string;
+    };
+  };
+}
 
 const Dashboard: React.FC = () => {
-  return <Container />;
+  const [gifs, setGifs] = useState<Gif[]>([]);
+  const [search, setSearch] = useState('');
+
+  async function handleSearch(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    try {
+      const results = await api.get('/search', {
+        params: {
+          api_key: 'crCvOcvLJWQMQJJyZEEZJdl21spihQXe',
+          q: `${search}`,
+        },
+      });
+      setGifs(results.data.data);
+    } catch (err) {
+      console.log('error');
+    }
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handleSearch}>
+        <input
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          placeholder="Pesquise um tema"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <div>
+        {gifs.map((gif: Gif) => (
+          <li key={gif.id}>
+            <img src={gif.images.fixed_height.url} alt="" />
+            <span>{gif.title}</span>
+          </li>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,50 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  background: black;
+  width: 100vw;
+  height: 100vh;
+`;
+
+interface FormProps {
+  hasError: boolean;
+}
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: none;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+    &:hover {
+    }
+  }
 `;
